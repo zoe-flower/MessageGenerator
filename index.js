207df41b745cf46068d729dc738b66520609cd55
zoe-flower <zoe.flower@justeattakeaway.com>
2025-03-11 15:23:15 +0000

    createMessage function to create descriptive names

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const prefix = ["miss", "mr", "mrs", "sir", "madam", "doctor"];
-const adjective = ["big", "small", "tall", "short", "fat", "thin"];
-const species = ["dog", "cat", "bird", "fish", "lizard", "snake"];
+const messageParts = {
+    prefix: ["miss", "mr", "mrs", "sir", "madam", "doctor"],
+    adjective: ["big", "small", "tall", "short", "fat", "thin"],
+    species: ["dog", "cat", "bird", "fish", "lizard", "snake"],
+}
+
+function createMessage() {
+    parts = Object.keys(messageParts)
+    message = [];
+    parts.forEach(part => {
+    const options = messageParts[part];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    message.push(options[randomIndex]);
+    });
+    console.log(message);
+}
+
+createMessage();
