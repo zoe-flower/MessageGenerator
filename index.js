9f1829c923742ff2f69f0de466e13e1ffa9fd3a6
zoe-flower <zoe.flower@justeattakeaway.com>
2025-03-11 15:26:48 +0000

    better property names

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const messageParts = {
+const descriptionCategories = {
     prefix: ["miss", "mr", "mrs", "sir", "madam", "doctor"],
     adjective: ["big", "small", "tall", "short", "fat", "thin"],
     species: ["dog", "cat", "bird", "fish", "lizard", "snake"],
 }
 
 function createMessage() {
-    parts = Object.keys(messageParts)
     message = [];
-    parts.forEach(part => {
-    const options = messageParts[part];
-    const randomIndex = Math.floor(Math.random() * options.length);
-    message.push(options[randomIndex]);
+    keys = Object.keys(descriptionCategories)
+    keys.forEach(key => {
+    const categoryOptions = descriptionCategories[key];
+    const randomIndex = Math.floor(Math.random() * categoryOptions.length);
+    message.push(categoryOptions[randomIndex]);
     });
     console.log(message);
 }
